d7bdaed308f62ced21fa1a1fbda767af2ce87b36
shubhada-kulkarni <shubhada.kulkarni@uni-heidelberg.de>
2025-08-18 11:12:46 +0200

    Fixed the stdout format and uuid fetching

--- submodules/run_cubitk_commands.py
+++ submodules/run_cubitk_commands.py
@@ -20,27 +20,25 @@ def create_landing_zone(project_id, capture_output=True, shell=True):
 
     Returns:
         Will return a UUID for landing zone that has been created
-        dict: Contains 'stdout', 'stderr', and 'exit_code'.
     """
     try:
         command = 'cubi-tk sodar landing-zone-create --sodar-url "https://sodar-test.internal/" --sodar-api-token "d1f108f3576bae3741e769c589189da7821d466dd77a3d0ec1593283f941b3b1" ' + project_id
-        print(command)
         result = subprocess.run(
             command,
             shell=shell,
             capture_output=capture_output,
             text=True
         )
-        if result["exit_code"] == 0:
+        if result.returncode == 0:
             print("Successfully created a landing zone.")
             # convert the json output to dictionary to fetch the landing zone uuid
-            json_acceptable_string = response["stdout"].replace("'", "\"")
+            json_acceptable_string = result.stdout.strip()
             out_dict = json.loads(json_acceptable_string)
             lz_uuid = out_dict['sodar_uuid']
             print(f"Created landing zone with UUID: {lz_uuid}")
             return(lz_uuid)
         else:
-            print("Landing zone could not be created. Please check:\n", response["stderr"])
+            print("Landing zone could not be created. Please check:\n", result.stderr.strip())
     except Exception as e:
         return {
             "stdout": "",
